3e2b1fdd337322c15f74870888ed081278b0cc68
Carmen Shing <carmenshing@gmail.com>
2018-07-27 21:05:47 +0000

    add working leaflet map

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
     def index
-        render 'application/index'
+        render 'application/map'
+    end
+
+    def @getLocation
+        navigator.geolocation.getCurrentPosition
+        showPostion
+    end
+
+    def showPosition(position)
+        x.innerHTML = [position.coords.latitude, position.coords.longitude]
     end
 end
